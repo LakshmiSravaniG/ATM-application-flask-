8568e40c4fa20e8e24e8b6f9bc24ec4dddf53525
LakshmiSravaniG <lsravanig@gmail.com>
2026-04-28 14:59:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, request, redirect, url_for, render_template, make_response, jsonify
+app = Flask(__name__)
+users = {}  # to store user data
+
+
+@app.route('/')
+def home():
+    return render_template('welcome.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        print(request.form)
+        username = request.form['username']
+        userpassword = request.form['password']
+        user_pinno = request.form['userpinno']
+        if username not in users:
+            users[username] = {'user_password': userpassword,
+                               'user_pinno': user_pinno, 'Amount': 0}
+            print(users)
+            return redirect(url_for('login'))
+        else:
+            return 'user already existed'
+
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login_username = request.form['username']
+        login_password = request.form['password']
+
+        if login_username in users:
+            if users[login_username]['user_password'] == login_password:
+                resp = make_response(redirect(url_for('dashboard')))
+                resp.set_cookie('user', login_username)
+                return resp
+            else:
+                return 'password wrong'
+        else:
+            return 'Username was wrong'
+    return render_template('login.html')
+
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    if request.cookies.get('user'):
+        return render_template('dashboard.html')
+    else:
+        return 'pls login view dashboard'
+
+
+@app.route('/deposit', methods=['GET', 'PUT'])
+def deposit():
+    if request.cookies.get('user'):
+        if request.method == 'PUT':
+            username = request.cookies.get('user')
+            print(request.get_json())
+            deposit_amount = int(request.get_json()['amount'])  # 500
+            if deposit_amount > 0:
+                if deposit_amount % 100 == 0:
+                    if deposit_amount <= 50000:
+                        users[username]['Amount'] = users[username]['Amount'] + \
+                            deposit_amount
+                        return f'{deposit_amount}'
+                    else:
+                        return jsonify({'message': 'Amount should be <= 50000'})
+                else:
+                    return jsonify({'message': 'Amount should be multiples of 100'})
+            else:
+                return jsonify({'message': 'Amount should be > 0'})
+        return render_template('deposit.html')
+    else:
+        return 'pls login view deposit'
+
+
+@app.route('/withdraw', methods=['GET', 'PUT'])
+def withdraw():
+    if request.cookies.get('user'):
+        if request.method == 'PUT':
+            username = request.cookies.get('user')
+            print(request.get_json())
+
+            withdraw_amount = int(request.get_json()['amount'])
+            balance_amount = users[username]['Amount']
+
+            if withdraw_amount > 0:
+                if withdraw_amount % 100 == 0:
+                    if withdraw_amount <= balance_amount:
+                        users[username]['Amount'] = balance_amount - \
+                            withdraw_amount
+                        return jsonify({'message': f'{users[username]['Amount']} after withdraw'})
+                    else:
+                        return jsonify({'message': 'Insufficient balance'})
+                else:
+                    return jsonify({'message': 'Amount should be multiples of 100'})
+            else:
+                return jsonify({'message': 'Amount should be > 0'})
+
+        return render_template('withdraw.html')
+
+    else:
+        return 'pls login view withdraw'
+
+
+@app.route('/balance', methods=['GET'])
+def balance():
+    if request.cookies.get('user'):
+        username = request.cookies.get('user')
+        balance_amount = users[username]['Amount']
+        return render_template('balance.html', balance_amount=balance_amount)
+    else:
+        return redirect(url_for('login'))
+
+
+app.run(use_reloader=True, debug=True)
